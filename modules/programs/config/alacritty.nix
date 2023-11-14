07f93f405ac7b0b2116fe71e195a1eed13b45344
Berkan E. <bee1850@thi.de>
2023-11-14 13:44:26 +0100

    Formatting

--- modules/programs/config/alacritty.nix
+++ modules/programs/config/alacritty.nix
@@ -1,4 +1,4 @@
 { config, pkgs, ... }:
 {
-    home.file.".config/alacritty/alacritty.yml".source = ./alacritty/alacritty.yml;
+  home.file.".config/alacritty/alacritty.yml".source = ./alacritty/alacritty.yml;
 }
