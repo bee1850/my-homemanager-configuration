945a57f7af007c3b64e445011cc806564588b5f9
Berkan Erkasap <bee1850@thi.de>
2023-11-14 12:33:22 +0100

    Update

--- modules/programs/config/nvim/keymappings.nix
+++ modules/programs/config/nvim/keymappings.nix
@@ -9,6 +9,27 @@
                     action = ":CHADopen<cr>";
                     options.desc = "Opens ChadTree";
                 }
+                      {
+        key = "<c-l>";
+        action = "<c-w>l";
+        options.desc = "Move to right window";
+      }
+      {
+        key = "<c-h>";
+        action = "<c-w>h";
+        options.desc = "Move to left window";
+      }
+      {
+        key = "<c-j>";
+        action = "<c-w>j";
+        options.desc = "Move to below window";
+      }
+      {
+        key = "<c-k>";
+        action = "<c-w>k";
+        options.desc = "Move to above window";
+      }
+
             ];
         
     };
