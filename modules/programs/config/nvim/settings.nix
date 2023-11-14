ca9ce7c5be2ca1c7714583c50a6930f7ea268855
Berkan E. <bee1850@thi.de>
2023-11-14 14:06:13 +0100

    Formatting

--- modules/programs/config/nvim/settings.nix
+++ modules/programs/config/nvim/settings.nix
@@ -4,7 +4,7 @@
     options = {
       number = true;
       relativenumber = true;
-      spell = true;
+      spell = false; # Has issues with CHADtree
       spelllang = [ "de" "en" ];
     };
     extraConfigVim = "let skip_defaults_vim=1\nset viminfo=\"\"";
